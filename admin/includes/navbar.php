31888d6cce2d0af525a9205fb6e306e43d90d7c8
cessrelopez <45307747+cessrelopez@users.noreply.github.com>
2019-02-09 14:52:59 +0800

    Add files via upload

---
+++ admin/includes/navbar.php
@@ -0,0 +1,10 @@
+<div class="header">
+	<div class="logo">
+		<a href="<?php echo BASE_URL .'admin/dashboard.php' ?>">
+			<h1 style="margin-right:2%;">Travel W/ Cess & Yana - Admin</h1>
+		</a>
+	</div>
+	<div class="user-info">
+		<span>Travel W/ Cess & Yana</span> &nbsp; &nbsp; <a href="<?php echo BASE_URL . '/logout.php'; ?>" class="logout-btn">logout</a>
+	</div>
+</div>
---
+++ includes/navbar.php
@@ -0,0 +1,11 @@
+<div class="navbar">
+			<div class="logo_div">
+				<a href="index.php"><h1>Travel W/ Cess & Yana</h1></a>
+			</div>
+			<ul>
+			  <li><a class="active" href="index.php">Home</a></li>
+			  <li><a href="#news">Places</a></li>
+			  <li><a href="#contact">Blog</a></li>
+			  <li><a href="#about">About</a></li>
+			</ul>
+		</div>
